f0af7eb97f71eedb40a5fa1594eb057bfca53001
Prost0Chudo <arty-arty@yandex.ru>
2022-01-21 17:31:52 +0300

    Create navigate.php

---
+++ navigate.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <nav>
+        <div class="menu">
+            <div class="image-svg">
+            <img src="images/ccfc35674a6649b6c0c170554b7d287b.svg">
+            </div>
+            <div class="menu-txt">
+            <ul><a href = "road.php">Посмотреть все<br> пожарно-спасательные гарнизоны<br><br> НАЖМИ НА МЕНЯ</a></ul>
+            </div>
+        </div>
+    </nav>
+</div>
+</header>
+</body>
+</html>
